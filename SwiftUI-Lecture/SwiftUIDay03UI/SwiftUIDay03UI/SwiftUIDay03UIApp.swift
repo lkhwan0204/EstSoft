59973b4f8a6134d221fd0cea4032a82626bc6841
lkhwan0204@gmail.com <lkhwan0204@gmail.com>
2025-02-12 19:34:42 +0900

    Fix: 강제 커밋

---
+++ SwiftUI-Lecture/SwiftUIDay03UI/SwiftUIDay03UI/SwiftUIDay03UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDay03UIApp.swift
+//  SwiftUIDay03UI
+//
+//  Created by kyuhyeon Lee on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDay03UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
